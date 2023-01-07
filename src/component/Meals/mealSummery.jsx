9b096afd0684a92c8d28d7f1eb55a4f3aa286591
ranaweysi <ranaweysi@gmail.com>
2023-01-07 23:43:13 +0330

    create mealSummery.js file

---
+++ src/component/Meals/mealSummery.jsx
@@ -0,0 +1,15 @@
+import classes from "./mealSummery.module.css";
+
+const MealSummery = () => {
+  return ( 
+    <section className={classes.summary}>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Placeat libero ab, facilis obcaecati vitae reiciendis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+           Fuga porro debitis repellendus.</p>
+    </section>
+   );
+}
+ 
+export default MealSummery;
