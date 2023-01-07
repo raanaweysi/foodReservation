740f7b538d87a50df32484cba59bf97c3fc0df66
ranaweysi <ranaweysi@gmail.com>
2023-01-07 23:42:07 +0330

    create mealItem.js file

---
+++ src/component/Meals/MealItem/Mealitem.jsx
@@ -0,0 +1,36 @@
+import catrContext from "../../../store/cartContext";
+import { Fragment, useContext } from "react";
+import classes from "./Mealitem.module.css";
+import MealItemForm from "../MealItem/mealItemForm";
+
+
+
+const MealItem = (props) => {
+
+  const cartCtx = useContext(catrContext);
+
+  const AddToCartHandler = amount => {
+    cartCtx.addItem({
+      id: props.id,
+      name:props.name,
+      amount: amount,
+      price: props.price
+    })
+  };
+
+  const price = (`$${props.price.toFixed(2)}`);
+  return ( 
+   <Fragment>
+    <li className={classes.meal}>
+    <div>
+      <h3>{props.name}</h3>
+      <div className={classes.description}>{props.description}</div>
+      <div className={classes.price}>{price}</div>
+    </div>
+    <div><MealItemForm onAddToCart={AddToCartHandler} /></div>
+    </li>
+   </Fragment>
+   );
+}
+ 
+export default MealItem;
