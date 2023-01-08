5630207721b61500c7bb8001c7af18d8aba5dac1
ranaweysi <ranaweysi@gmail.com>
2023-01-08 21:42:07 +0330

    added cartItem

---
+++ src/component/Cart/cartItem.jsx
@@ -0,0 +1,20 @@
+import classes from './cartItem.modules.css';
+
+
+const CartItem = (props) => {
+  return ( 
+    <li>
+      <div>
+        <h2></h2>
+        <span></span>
+        <span></span>
+      </div>
+      <div>
+        <button></button>
+        <button></button>
+      </div>
+    </li>
+   );
+}
+ 
+export default CartItem;
