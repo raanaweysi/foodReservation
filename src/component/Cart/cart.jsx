fae077704224cf2d78f2b244719d55a8b3da3453
ranaweysi <ranaweysi@gmail.com>
2023-01-08 21:48:01 +0330

    Receive item from context

--- src/component/Cart/cart.jsx
+++ src/component/Cart/cart.jsx
@@ -1,10 +1,20 @@
 import Modal from "../UI/Modal";
 import calsses from "./cart.module.css";
+import CartContext from "../../store/cartContext";
+import { useContext } from "react";
 
 const Cart = (props) => {
-  const cartItem = [{ id: "c1", name: "sushi", amount: 2, price: 56.99 }].map(
-    (item,i) => {<li>{item.name}</li>} 
-  );
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
+  const hasItem = cartCtx.items.length > 0;
+
+  const cartItem = <ul> {cartCtx.items.map(
+    (item,i) => <li>{item.name}</li>
+  )}
+  </ul>
+  ;
 
   
   return (
@@ -12,11 +22,11 @@ const Cart = (props) => {
       {cartItem}
       <div className={calsses.total}>
         <span>Total Amount</span>
-        <span>56.99</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={calsses.actions}>
         <button onClick={props.onClose} className={calsses['button--alt']}>Close</button>
-        <button className={calsses.button}>Order</button>
+       {hasItem  && <button className={calsses.button}>Order</button>}
       </div>
     </Modal>
   );
