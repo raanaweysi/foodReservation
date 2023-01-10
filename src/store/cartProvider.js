7bc49eba991e767851d93862a8687b9c3aeca2f2
ranaweysi <ranaweysi@gmail.com>
2023-01-10 11:31:58 +0330

    set dispatch for reducer increse and decrese

--- src/store/cartProvider.js
+++ src/store/cartProvider.js
@@ -8,14 +8,53 @@ const defaultCartState = {
 };
 
 const cartReducer = (state,action) => {
+  
   if(action.type === "ADD_ITEM"){
-    const updateItem = state.items.concat(action.item);    
     const updateTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+    const existingItemCartIndex = state.items.findIndex(
+      item => item.id === action.item.id);
+    const existingItemCart = state.items[existingItemCartIndex];
+    let updateItems;
+  
+    if(existingItemCart) {
+      const updateItem = {
+        ...existingItemCart,
+        amount: existingItemCart.amount + action.item.amount
+      };
+      updateItems = [...state.items];
+      updateItems[existingItemCartIndex] = updateItem;
+    } else {
+  
+      updateItems = state.items.concat(action.item);   
+    };
     return {
-      items : updateItem,
+      items : updateItems,
       totalAmount: updateTotalAmount
     };
   };
+  if(action.type === 'REMOVE'){
+    const existingItemIndex = state.items.findIndex(
+      item => item.id === action.id);
+      const existingItem = state.items[existingItemIndex];
+      const updateTotalAmount = state.totalAmount - existingItem.price;
+    let updateItems;
+
+    if(existingItem.amount === 1) {
+     updateItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      const updateItem = {
+        ...existingItem,
+        amount: existingItem.amount - 1
+      };
+      updateItems = [...state.items];
+      updateItems[existingItemIndex]= updateItem;
+    }
+    return {
+      items: updateItems,
+      totalAmount: updateTotalAmount
+    }
+  }
   return defaultCartState;
 };
  
@@ -23,12 +62,12 @@ const CartProvider = (props) => {
 
   const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
-  const addItemToCartHandler = (item) => {
+  const addItemToCartHandler = item => {
     dispatchCartAction({type: "ADD_ITEM", item: item})
   };
 
 
-  const removeItemToCartHandler = (id) => {
+  const removeItemToCartHandler = id => {
     dispatchCartAction({type: "REMOVE", id: id})
   };
 
