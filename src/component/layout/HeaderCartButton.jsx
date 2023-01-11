5f826e8e433c5ab6d5df02a208cf5854b6e79c87
ranaweysi <ranaweysi@gmail.com>
2023-01-11 10:50:38 +0330

    Implementation class bump

--- src/component/layout/HeaderCartButton.jsx
+++ src/component/layout/HeaderCartButton.jsx
@@ -1,19 +1,39 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cartContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 
 
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 
   const numberOfCartContext = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount
   }, 0);
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(()=>{
+    if(cartCtx.items.length === 0){
+      return
+    }
+    setBtnIsHighlighted(true);
+
+    
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false)
+    }, 300);
+
+    return()=>{
+      clearTimeout(timer)
+    }
+  },[cartCtx]);
+
   return ( 
-    <button onClick={props.onclick} className={classes.button}>
+    <button onClick={props.onclick} className={btnClasses}>
       <span className={classes.icon}><CartIcon/></span>
       <span>your cart</span>
       <span className={classes.badge}>{numberOfCartContext}</span>
